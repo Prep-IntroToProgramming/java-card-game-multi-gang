5e0d4be7ae61bb73963bf103fddb0fc29c9eed27
Daniel Comer <19ComerD@flintridgeprep.org>
2018-11-20 10:43:13 -0800

    Formatting

--- TestCard.java
+++ TestCard.java
@@ -5,29 +5,38 @@ public class TestCard {
     public static void main(String[] args){
         int testRank = Card.FOUR;
         int testSuit = Card.HEARTS;
+        
         //Makes sure Card is working properly
         Card c1 = new Card(testRank, testSuit);
         System.out.println(c1.cardName);
+        
         //Tests draw() function
         Deck d1 = new Deck();
         System.out.println(d1.draw().cardName);
         System.out.println(d1.count());
+        
         //Resets deck
         d1.resetDeck();
         System.out.println(d1.count());
+        
         //Tests draw(int)
         System.out.println(d1.draw(20).cardName);
         System.out.println(d1.count());
+        
         d1.resetDeck();
+        
         //Tests drawRand
         System.out.println(d1.drawRand().cardName);
         System.out.println(d1.count());
+        
         d1.resetDeck();
+        
         //Tests the check functions and discard
         System.out.println(d1.check(new Card(Card.ACE, Card.DIAMONDS)));
         d1.discard();
-        System.out.println(d1.check(new Card(Card.ACE, Card.DIAMONDS)));
-        
+        System.out.println(d1.check(Card.ACE, Card.DIAMONDS));
+        System.out.println(d1.checkSuit(Card.DIAMONDS));
+        System.out.println(d1.checkRank(Card.ACE));
         
     }  
 }
